a152bad3007adce4067e2a34d4287491af88456e
Karuna Grewal <karunagrewal98@gmail.com>
2019-02-14 19:47:15 +0530

    Parse cmds into a container

--- m_shell_parse.c
+++ m_shell_parse.c
@@ -1,37 +1,65 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "m_shell_parse.h"
 
-/**
-* Change the allocation to bufsize
-* Support Reallocation.
-*/
-char ***
-tokenize_cmd(char *cmd)
+static struct cmd_entry*
+new_cmd_entry(char *cmd_ptr, struct cmd_entry *next_cmd_entry)
+{
+    struct cmd_entry *temp = (struct cmd_entry*) malloc(sizeof(temp));
+    temp->cmd = cmd_ptr;
+    temp->cmd_next = NULL;
+    if (next_cmd_entry != NULL) {
+        next_cmd_entry->cmd_next = temp;
+    }
+    return temp;
+}
+
+static struct cmd_cointainer*
+new_cmd_container()
+{
+    struct cmd_cointainer *temp = (struct cmd_cointainer*) malloc(sizeof(temp));
+    temp->cmd_list = NULL;
+    temp->cmd_tail = NULL;
+    temp->cmd_count = 0;
+    return temp;
+}
+
+static void
+cmd_cointainer_insert(struct cmd_cointainer *cmd_list_cnt, char *cmd_ptr)
+{
+    struct cmd_entry *temp_cmd_entry = new_cmd_entry(cmd_ptr, cmd_list_cnt->cmd_tail);
+    if (cmd_list_cnt->cmd_list == NULL)
+        cmd_list_cnt->cmd_list = temp_cmd_entry;
+    cmd_list_cnt->cmd_tail = temp_cmd_entry;
+    cmd_list_cnt->cmd_count += 1;
+}
+
+static void
+cmd_cointainer_print(struct cmd_cointainer *cmd_list_cnt)
+{
+    struct cmd_entry *iter = cmd_list_cnt->cmd_list;
+    int index = 1;
+    while(iter)
+    {
+        printf("%d :%s\n", index++, iter->cmd);
+        iter = iter->cmd_next;
+    }
+}
+
+// FREE : after executing the cmds mentioned in the container.
+struct cmd_cointainer*
+cmd_tokenize(char *cmd)
 {
     int count = 0;
-    char **cmd_list = (char **) malloc(4*sizeof(char *));
-    char ***cmd_index = (char ***) malloc(4*sizeof(char *));
-    while((*(cmd_list + count) = strsep(&cmd, "|")) != NULL)
+    char *curr_cmd;
+    char *cmd_dup = cmd;
+    //FREE cmd_list if count == 0 ; But that won't be a case ;)
+    struct cmd_cointainer *cmd_list = new_cmd_container();
+    while((curr_cmd = strsep(&cmd_dup, "|")) != NULL)
     {
         count++;
+        //insert this cmd into the container
+        cmd_cointainer_insert(cmd_list, curr_cmd);
     }
-    for(int i = 0; i < count; i++)
-    {
-        int args = 0;
-        char **ptr = (char **)malloc(10 * sizeof(char *));
-        while((ptr[args] = strsep(&cmd_list[i], " \t")) != NULL)
-        {
-            if (strlen(ptr[args]) > 0)
-                args++;
-        }
-        //free cmd_ptr
-        cmd_index[i] = ptr;
-        printf("\n%d: %s", i+1);
-        for(int j = 0; j < args; j++)
-        {
-            printf("%s,", cmd_index[i][j]);
-        }
-    }
-    //free ptrs in cmd_index.
+    return cmd_list;
 }
--- m_shell_parse.h
+++ m_shell_parse.h
@@ -1,6 +1,23 @@
 #ifndef _M_SHELL_PARSE_H_
 #define _M_SHELL_PARSE_H_
 
-char *** tokenize_cmd(char *cmd);
+#define CMD_NR 5
+char*** tokenize_cmd(char *cmd);
+
+struct cmd_entry{
+    /**
+    * array of ptr to the args of the given command
+    */
+    char* cmd;
+    struct cmd_entry* cmd_next;
+};
+
+struct cmd_cointainer{
+    struct cmd_entry *cmd_list;
+    struct cmd_entry *cmd_tail;
+    int cmd_count;
+};
+
+struct cmd_cointainer*  cmd_tokenize(char *cmd);
 
 #endif
