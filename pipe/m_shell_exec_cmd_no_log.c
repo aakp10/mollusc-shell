1fb2c7cdcf4c72a0105f07ea0e8dab97ce7e82f0
Karuna Grewal <karunagrewal98@gmail.com>
2019-02-17 11:39:12 +0530

    Log works for 1 pipe

--- pipe/m_shell_exec_cmd_no_log.c
+++ pipe/m_shell_exec_cmd_no_log.c
@@ -11,6 +11,8 @@
 #include "m_shell_log.h"
 #include <time.h>
 #include <limits.h>
+#include <errno.h>
+
 void
 cmd_exec_pipe(struct cmd_container *cmd_list_cnt, int logging)
 {
@@ -22,7 +24,8 @@ cmd_exec_pipe(struct cmd_container *cmd_list_cnt, int logging)
     int stream_nav = -1;
     char *cmd_names[cmd_count];
     pid_t cmd_pid[cmd_count];
-    int ip_stream, op_stream;
+    int int_pipe[2];
+    pipe(int_pipe);
     for(int i = 0; i < pipe_count; i += 2)
     {
         if (pipe(pipefds + i) == -1) {
@@ -45,34 +48,83 @@ cmd_exec_pipe(struct cmd_container *cmd_list_cnt, int logging)
         }
         *(arg_list + count) = NULL;
         cmd_names[index - 1] = arg_list[0];
+
+        if(index!= 1 && fork() == 0)
+        {
+            int len, slen;
+            int fd = open("output.log", O_RDWR | O_CREAT| O_TRUNC, S_IRUSR | S_IWUSR);
+            write(fd, cmd_names[index - 1], strlen(cmd_names[index - 1]));
+            close(int_pipe[0]);
+            do {
+                close(pipefds[j-1]);
+                len = tee(pipefds[j-2], int_pipe[1],
+                            INT_MAX, SPLICE_F_NONBLOCK);
+                if (len < 0) {
+                    if (errno == EAGAIN)
+                    {
+                        printf("again");
+                        continue;
+                    }
+                    perror("hitee");
+                    exit(EXIT_FAILURE);
+                } else
+                    if (len == 0)
+                        break;
+                    //read(int_pipe, buf, INT_MAX);
+                    //write(fd, buf, strlen(buf));
+                    while (len > 0) {
+                    slen = splice(pipefds[j-2], NULL, fd, NULL,
+                                    len, SPLICE_F_MOVE);
+                    if (slen < 0) {
+                        perror("splice");
+                        break;
+                    }
+                    len -= slen;
+                }
+            } while (1);
+            close(fd);
+            close(int_pipe[1]);
+            for(int i = 0; i < pipe_count; i++)
+            {
+                close(pipefds[i]);
+            }
+            exit(0);
+        }
+
+
         if ((cmd_pid[index - 1] = fork()) == 0) {
-            
             if (index == 1)
             {
                 if (index != cmd_list_cnt->cmd_count)
                 {
-                    op_stream = pipefds[j+1];
-                    dup2(op_stream, 1);
+                    //op_stream = ;
+                    dup2(pipefds[j+1], 1);
+                    
                 }
-                
+                close(int_pipe[1]);
+                close(int_pipe[0]);
             }
             else if (index != cmd_list_cnt->cmd_count) {
                 close(pipefds[j-1]);
+                //dup2(pipefds[j-2], 0);
+                dup2(int_pipe[0], 0);
                 
-                dup2(pipefds[j-2], 0);
-                op_stream = pipefds[j+1];
-                dup2(op_stream, 1);
+                close(int_pipe[1]);
+                close(int_pipe[0]);
+                //op_stream = ;
+                dup2(pipefds[j+1], 1);
             }
             else if (index == cmd_list_cnt->cmd_count) {
-
-                dup2(pipefds[j-2], 0);
+                close(int_pipe[1]);
+                dup2(int_pipe[0], 0);
+                //dup2(pipefds[j-2], 0);
+                close(int_pipe[0]);
             }
 
             for(int i = 0; i < pipe_count; i++)
             {
                 close(pipefds[i]);
             }
-           
             execvp(arg_list[0], arg_list);
             exit(-1);
         }
@@ -86,6 +138,8 @@ cmd_exec_pipe(struct cmd_container *cmd_list_cnt, int logging)
     {
         close(pipefds[i]);
     }
+    close(int_pipe[1]);
+    close(int_pipe[0]);
     pid_t pid;
     int status;
     while((pid = wait(&status)) != -1)
