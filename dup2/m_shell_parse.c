7a1836bfc1d79e61d35fdbb2d5debb5392c4e407
Karuna Grewal <karunagrewal98@gmail.com>
2019-02-17 10:52:54 +0530

    dup2 version of shell

---
+++ dup2/m_shell_parse.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "m_shell_parse.h"
+#include <string.h>
+static struct cmd_entry*
+new_cmd_entry(char *cmd_ptr, struct cmd_entry *next_cmd_entry)
+{
+    struct cmd_entry *temp = (struct cmd_entry*) malloc(sizeof(temp));
+    temp->cmd = cmd_ptr;
+    temp->cmd_next = NULL;
+    if (next_cmd_entry != NULL) {
+        next_cmd_entry->cmd_next = temp;
+    }
+    return temp;
+}
+
+static struct cmd_container*
+new_cmd_container()
+{
+    struct cmd_container *temp = (struct cmd_container*) malloc(sizeof(temp));
+    temp->cmd_list = NULL;
+    temp->cmd_tail = NULL;
+    temp->cmd_count = 0;
+    return temp;
+}
+
+static void
+cmd_container_insert(struct cmd_container *cmd_list_cnt, char *cmd_ptr)
+{
+    struct cmd_entry *temp_cmd_entry = new_cmd_entry(cmd_ptr, cmd_list_cnt->cmd_tail);
+    if (cmd_list_cnt->cmd_list == NULL)
+        cmd_list_cnt->cmd_list = temp_cmd_entry;
+    cmd_list_cnt->cmd_tail = temp_cmd_entry;
+    cmd_list_cnt->cmd_count += 1;
+}
+
+void
+cmd_container_print(struct cmd_container *cmd_list_cnt)
+{
+    struct cmd_entry *iter = cmd_list_cnt->cmd_list;
+    int index = 1;
+    while(iter)
+    {
+        printf("%d :%s\n", index++, iter->cmd);
+        iter = iter->cmd_next;
+    }
+}
+
+// FREE : after executing the cmds mentioned in the container.
+struct cmd_container*
+cmd_tokenize(char *cmd)
+{
+    int count = 0;
+    char *curr_cmd;
+    char *cmd_dup = cmd;
+    //FREE cmd_list if count == 0 ; But that won't be a case ;)
+    struct cmd_container *cmd_list = new_cmd_container();
+    while((curr_cmd = strsep(&cmd_dup, "|")) != NULL)
+    {
+        count++;
+        //insert this cmd into the container
+        cmd_container_insert(cmd_list, curr_cmd);
+    }
+    return cmd_list;
+}
